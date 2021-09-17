aca8ce194fedfcf3a70653a947209c53fc47a4c1
VulcanBin <58626386+MWaleedK@users.noreply.github.com>
2021-09-17 22:23:59 +0500

    Add files via upload

---
+++ myWebApp/imgUpload/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+if __name__=='__main__':
+    pass
---
+++ myWebApp/imgUpload/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+if __name__=='__main__':
+    pass
